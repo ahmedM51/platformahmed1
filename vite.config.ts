82d199ee8bb18caa6c331481ddf273baf85baee3
omergehad405 <omergehad593@gmail.com5>
2026-01-12 19:12:37 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -10,7 +9,7 @@ export default defineConfig(({ mode }) => {
   const basePath = isGitHubPages ? '/platformahmed1/' : '/';
 
   return {
-    plugins: [react(), tailwindcss(),],
+    plugins: [react(), ],
     // ✅ FIXED: Use basePath correctly
     base: basePath,
     define: {
