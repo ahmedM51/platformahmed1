3104acab166918de8d11612ced71d1267e5677e2
ahmedM51 <ahmedmohamed4336@gmail.com>
2026-01-13 04:21:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {
-      API_KEY: JSON.stringify(process.env.API_KEY || ''),
-      SUPABASE_URL: JSON.stringify(process.env.SUPABASE_URL || 'https://pxmhwwovxrnefiryywva.supabase.co'),
-      SUPABASE_ANON_KEY: JSON.stringify(process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bWh3d292eHJuZWZpcnl5d3ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MzgzNjQsImV4cCI6MjA3MjAxNDM2NH0.FqzkWel93icaJ781ZCPhvzfVJu4iwqCa3hxV3AKuRlA')
-    },
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || 'AIzaSyD3YHJAyUwBCIBLewp6II8Uqbu3gPkgy5Q'),
+    'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL || 'https://pxmhwwovxrnefiryywva.supabase.co'),
+    'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bWh3d292eHJuZWZpcnl5d3ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MzgzNjQsImV4cCI6MjA3MjAxNDM2NH0.FqzkWel93icaJ781ZCPhvzfVJu4iwqCa3hxV3AKuRlA'),
     'global': 'window'
   },
   server: {
