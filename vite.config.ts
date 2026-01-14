4a16732dd953f0799931b6919e04193fd2f4333a
ahmedM51 <ahmedmohamed4336@gmail.com>
2026-01-14 03:50:18 +0200

    Update API_KEY in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     // Vite replaces these values during build time
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || 'AIzaSyBDReabbWtJPI31sPz6XHZ-vWT19vY7jCI'),
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || 'AIzaSyCQnAWlCFC5FIucZA1D9pPGPYY_sXPhg_k'),
     'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL || 'https://pxmhwwovxrnefiryywva.supabase.co'),
     'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bWh3d292eHJuZWZpcnl5d3ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MzgzNjQsImV4cCI6MjA3MjAxNDM2NH0.FqzkWel93icaJ781ZCPhvzfVJu4iwqCa3hxV3AKuRlA'),
     'global': 'window',
