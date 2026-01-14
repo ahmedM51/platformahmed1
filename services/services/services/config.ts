52727822ce5674b6873efb6a2c016782ddc4b9b0
ahmedM51 <ahmedmohamed4336@gmail.com>
2026-01-14 07:03:49 +0200

    Delete services/services/services directory

--- services/services/services/config.ts
+++
@@ -1,7 +0,0 @@
-
-export const CONFIG = {
-  GEMINI_API_KEY: 'AIzaSyBNkAIB1P5lFmfrBKTCf9oPtSxcWvJCBpw',
-  SUPABASE_URL: 'https://cmaxutqmblvvghftouqx.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bWh3d292eHJuZWZpcnl5d3ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MzgzNjQsImV4cCI6MjA3MjAxNDM2NH0.FqzkWel93icaJ781ZCPhvzfVJu4iwqCa3hxV3AKuRlA',
-  IS_DEV: window.location.hostname === 'localhost'
-};
