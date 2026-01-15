3e66656f033f98871dc6627a87613053818c5d78
ahmedM51 <ahmedmohamed4336@gmail.com>
2026-01-15 14:29:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  // تحميل متغيرات البيئة بناءً على الوضع (development أو production)
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    define: {
-      // تمرير المفتاح ليكون متاحاً في الكود البرمجي
-      'process.env.API_KEY': JSON.stringify(env.API_KEY || env.VITE_API_KEY),
-      'global': 'window',
-    },
-    build: {
-      outDir: 'dist',
-      sourcemap: false,
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    chunkSizeWarningLimit: 2000,
+  },
 });
